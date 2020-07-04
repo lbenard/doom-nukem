6e23cd55fbbfb3e315cab576471b0ce35c9819fc
Lucas BENARD <lbenard@student.42.fr>
2020-07-04 23:27:56 +0200

    forgot delta header

--- includes/engine/delta.h
+++ includes/engine/delta.h
@@ -6,17 +6,22 @@
 /*   By: lbenard <lbenard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/28 17:47:02 by lbenard           #+#    #+#             */
-/*   Updated: 2019/10/04 12:05:28 by lbenard          ###   ########.fr       */
+/*   Updated: 2020/07/04 23:27:43 by lbenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef DELTA_H
 # define DELTA_H
 
+# define MAX_DELTA 0.0333333333f
+
 /*
 ** Time management.
 ** Delta time gives the time needed to render the last frame, so that any
 ** time-dependant update can scale to the speed of the game.
+** The MAX_DELTA value defines the maximum value that the delta can hold. It
+** helps preventing issues related to performances issues that could lead to
+** unexpected big delta values and results
 */
 void	set_last_delta(const float delta);
 float	get_last_delta(void);
