45ab79c00493bcbba3c7fcf3581f74d88be87300
Lucas BENARD <lbenard@student.42.fr>
2020-08-10 21:35:33 +0200

    mouse is now turned back into visible state in script destructor

--- srcs/game/scenes/list/script/destroy.c
+++ srcs/game/scenes/list/script/destroy.c
@@ -6,15 +6,17 @@
 /*   By: lbenard <lbenard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/29 01:37:18 by lbenard           #+#    #+#             */
-/*   Updated: 2020/07/29 17:15:09 by lbenard          ###   ########.fr       */
+/*   Updated: 2020/08/05 23:54:39 by lbenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "game/scenes/script_scene.h"
+#include "game/game.h"
 
 void	destroy_script_scene(t_script_scene *const self)
 {
+	cursor_set_visibility(&self->window->cursor, TRUE);
 	free(self->writing_paragraph1);
 	free(self->writing_paragraph2);
 	destroy_scene(&self->super);
