d46d52dc036603bae19c97c34258dce631295d88
ppetiteau <ppetiteau@apis-artis.com>
2020-08-17 21:10:57 +0200

    norme

--- srcs/game/entities/list/editor/block_checkbox/init.c
+++ srcs/game/entities/list/editor/block_checkbox/init.c
@@ -13,26 +13,19 @@
 #include "game/entities/editor/block_checkbox_entity.h"
 #include "engine/error.h"
 
-t_result	init_block_checkbox_entity(t_block_checkbox_entity *const self,
-				const t_block_checkbox_entity_args *const args)
+static t_result	init_block_checkbox_entity3(t_block_checkbox_entity *const self)
 {
-	if (static_module_create(self, preview_checkbox_entity(args->base_name,
-		&args->block->texture, args->window)) == ERROR)
+	if (self->super.super.super.module.has_error)
 	{
+		destroy_block_checkbox_entity(self);
 		return (throw_result_str("init_block_checkbox_entity()",
-			"failed to create parent object"));
+			"failed to create block checkbox entity"));
 	}
-	self->block = args->block;
-	frame_layer_transform_add(&self->super.super.normal_texture,
-		&self->block->texture,
-		ft_frame_transform(ft_vec2f(0.5f, 0.5f),
-			ft_isize(self->super.super.normal_texture.size.x / 2,
-				self->super.super.normal_texture.size.y / 2),
-			ft_vec2f((float)self->super.super.normal_texture.size.x
-				/ (float)self->block->texture.size.x * 0.7,
-				(float)self->super.super.normal_texture.size.y
-					/ (float)self->block->texture.size.y * 0.7),
-			255));
+	return (OK);
+}
+
+static void		init_block_checkbox_entity2(t_block_checkbox_entity *const self)
+{
 	frame_layer_transform_add(&self->super.super.hover_texture,
 		&self->block->texture,
 		ft_frame_transform(ft_vec2f(0.5f, 0.5f),
@@ -53,11 +46,28 @@ t_result	init_block_checkbox_entity(t_block_checkbox_entity *const self,
 				(float)self->super.super.checked_texture.size.y
 					/ (float)self->block->texture.size.y * 0.7),
 			255));
-	if (self->super.super.super.module.has_error)
+}
+
+t_result		init_block_checkbox_entity(t_block_checkbox_entity *const self,
+					const t_block_checkbox_entity_args *const args)
+{
+	if (static_module_create(self, preview_checkbox_entity(args->base_name,
+		&args->block->texture, args->window)) == ERROR)
 	{
-		destroy_block_checkbox_entity(self);
 		return (throw_result_str("init_block_checkbox_entity()",
-			"failed to create block checkbox entity"));
+			"failed to create parent object"));
 	}
-	return (OK);
+	self->block = args->block;
+	frame_layer_transform_add(&self->super.super.normal_texture,
+		&self->block->texture,
+		ft_frame_transform(ft_vec2f(0.5f, 0.5f),
+			ft_isize(self->super.super.normal_texture.size.x / 2,
+				self->super.super.normal_texture.size.y / 2),
+			ft_vec2f((float)self->super.super.normal_texture.size.x
+				/ (float)self->block->texture.size.x * 0.7,
+				(float)self->super.super.normal_texture.size.y
+					/ (float)self->block->texture.size.y * 0.7),
+			255));
+	init_block_checkbox_entity2(self);
+	return (init_block_checkbox_entity3(self));
 }
