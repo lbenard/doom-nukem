ef673d6d39c0769f3de2917d6dbac53dd27f7ed7
ppetiteau <ppetiteau@apis-artis.com>
2020-08-17 21:21:21 +0200

    norme

--- srcs/game/entities/list/editor/entity_checkbox/init.c
+++ srcs/game/entities/list/editor/entity_checkbox/init.c
@@ -13,26 +13,21 @@
 #include "game/entities/editor/entity_checkbox_entity.h"
 #include "engine/error.h"
 
-t_result	init_entity_checkbox_entity(t_entity_checkbox_entity *const self,
-				t_entity_checkbox_entity_args *const args)
+static t_result	init_entity_checkbox_entity3(
+					t_entity_checkbox_entity *const self)
 {
-	if (static_module_create(self,
-		checkbox_entity_base(args->base_name, args->window)) == ERROR)
+	if (self->super.super.module.has_error)
 	{
+		destroy_entity_checkbox_entity(self);
 		return (throw_result_str("init_entity_checkbox_entity()",
-			"failed to create parent object"));
+			"failed to create entity checkbox entity"));
 	}
-	self->entity = args->entity;
-	frame_layer_transform_add(&self->super.normal_texture,
-		&self->entity->icon,
-		ft_frame_transform(ft_vec2f(0.5f, 0.5f),
-			ft_isize(self->super.normal_texture.size.x / 2,
-				self->super.normal_texture.size.y / 2),
-			ft_vec2f((float)self->super.normal_texture.size.x
-				/ (float)self->entity->icon.size.x * 0.7,
-				(float)self->super.normal_texture.size.y
-					/ (float)self->entity->icon.size.y * 0.7),
-			255));
+	return (OK);
+}
+
+static void		init_entity_checkbox_entity2(
+					t_entity_checkbox_entity *const self)
+{
 	frame_layer_transform_add(&self->super.hover_texture,
 		&self->entity->icon,
 		ft_frame_transform(ft_vec2f(0.5f, 0.5f),
@@ -53,11 +48,29 @@ t_result	init_entity_checkbox_entity(t_entity_checkbox_entity *const self,
 				(float)self->super.checked_texture.size.y
 					/ (float)self->entity->icon.size.y * 0.7),
 			255));
-	if (self->super.super.module.has_error)
+}
+
+t_result		init_entity_checkbox_entity(
+					t_entity_checkbox_entity *const self,
+					t_entity_checkbox_entity_args *const args)
+{
+	if (static_module_create(self,
+		checkbox_entity_base(args->base_name, args->window)) == ERROR)
 	{
-		destroy_entity_checkbox_entity(self);
 		return (throw_result_str("init_entity_checkbox_entity()",
-			"failed to create entity checkbox entity"));
+			"failed to create parent object"));
 	}
-	return (OK);
+	self->entity = args->entity;
+	frame_layer_transform_add(&self->super.normal_texture,
+		&self->entity->icon,
+		ft_frame_transform(ft_vec2f(0.5f, 0.5f),
+			ft_isize(self->super.normal_texture.size.x / 2,
+				self->super.normal_texture.size.y / 2),
+			ft_vec2f((float)self->super.normal_texture.size.x
+				/ (float)self->entity->icon.size.x * 0.7,
+				(float)self->super.normal_texture.size.y
+					/ (float)self->entity->icon.size.y * 0.7),
+			255));
+	init_entity_checkbox_entity2(self);
+	return (init_entity_checkbox_entity3(self));
 }
