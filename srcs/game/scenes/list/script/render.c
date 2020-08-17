c138e3f5227febc81fabbf5cb548f4983d1d1ed9
ppetiteau <ppetiteau@apis-artis.com>
2020-08-17 21:53:03 +0200

    norme

--- srcs/game/scenes/list/script/render.c
+++ srcs/game/scenes/list/script/render.c
@@ -15,42 +15,9 @@
 #include "maths/maths.h"
 #include "ft/str.h"
 
-void	script_scene_render(t_script_scene *const self,
-			t_frame *const fb)
+static void	script_scene_render3(t_script_scene *const self,
+				t_frame *const fb, t_bool is_over, float tig_fade)
 {
-	t_bool	is_over;
-	float	tig_fade;
-
-	is_over = ft_strlen(self->writing_paragraph2) == ft_strlen(self->paragraph2);
-	if (!is_over)
-		tig_fade = 0.0f;
-	else
-		tig_fade = ft_fmin((get_wall_time() - self->text_finished_time) / 8.0f,
-		1.0f);
-	if (!is_over)
-		frame_fill_blend(fb, ft_rgba(0, 0, 0, 1), blend_add);
-	else
-		frame_fill_blend(fb, ft_rgba(0, 0, 0, 255), blend_add);
-	text_render(&self->paragraph1_text, ft_text_settings(ft_isize(0, 0), 9));
-	text_render(&self->paragraph2_text, ft_text_settings(ft_isize(0, 0), 9));
-	frame_fill_blend(&self->paragraph1_text.target,
-		ft_rgba(255, 255, 255, 255),
-		blend_colorize);
-	frame_fill_blend(&self->paragraph2_text.target,
-		ft_rgba(255, 255, 255, 255),
-		blend_colorize);
-	frame_layer_transform_add(fb,
-		&self->paragraph1_text.target,
-		ft_frame_transform(ft_vec2f(0.5f, 0.5f),
-			ft_isize(fb->size.x / 2, fb->size.y / 2 - 50 - tig_fade * 250),
-			ft_vec2f(2.0f, 2.0f),
-			255));
-	frame_layer_transform_add(fb,
-		&self->paragraph2_text.target,
-		ft_frame_transform(ft_vec2f(0.5f, 0.5f),
-			ft_isize(fb->size.x / 2, fb->size.y / 2 + 50 - tig_fade * 250),
-			ft_vec2f(2.0f, 2.0f),
-			255));
 	if (is_over)
 	{
 		if (self->text_finished_time == 0.0)
@@ -68,4 +35,50 @@ void	script_scene_render(t_script_scene *const self,
 			ft_isize(fb->size.x - 10, fb->size.y - 10),
 			ft_vec2f(2.0f, 2.0f),
 			127));
+}
+
+static void	script_scene_render2(t_script_scene *const self,
+				t_frame *const fb, float tig_fade)
+{
+	frame_fill_blend(&self->paragraph2_text.target,
+		ft_rgba(255, 255, 255, 255),
+		blend_colorize);
+	frame_layer_transform_add(fb,
+		&self->paragraph1_text.target,
+		ft_frame_transform(ft_vec2f(0.5f, 0.5f),
+			ft_isize(fb->size.x / 2, fb->size.y / 2 - 50 - tig_fade * 250),
+			ft_vec2f(2.0f, 2.0f),
+			255));
+	frame_layer_transform_add(fb,
+		&self->paragraph2_text.target,
+		ft_frame_transform(ft_vec2f(0.5f, 0.5f),
+			ft_isize(fb->size.x / 2, fb->size.y / 2 + 50 - tig_fade * 250),
+			ft_vec2f(2.0f, 2.0f),
+			255));
+}
+
+void		script_scene_render(t_script_scene *const self,
+				t_frame *const fb)
+{
+	t_bool	is_over;
+	float	tig_fade;
+
+	is_over = ft_strlen(self->writing_paragraph2) ==
+		ft_strlen(self->paragraph2);
+	if (!is_over)
+		tig_fade = 0.0f;
+	else
+		tig_fade = ft_fmin((get_wall_time() - self->text_finished_time) / 8.0f,
+		1.0f);
+	if (!is_over)
+		frame_fill_blend(fb, ft_rgba(0, 0, 0, 1), blend_add);
+	else
+		frame_fill_blend(fb, ft_rgba(0, 0, 0, 255), blend_add);
+	text_render(&self->paragraph1_text, ft_text_settings(ft_isize(0, 0), 9));
+	text_render(&self->paragraph2_text, ft_text_settings(ft_isize(0, 0), 9));
+	frame_fill_blend(&self->paragraph1_text.target,
+		ft_rgba(255, 255, 255, 255),
+		blend_colorize);
+	script_scene_render2(self, fb, tig_fade);
+	script_scene_render3(self, fb, is_over, tig_fade);
 }
