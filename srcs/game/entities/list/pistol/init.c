f3d4dfcddc386726eee6992e102e44e97fb59afd
ppetiteau <ppetiteau@apis-artis.com>
2020-08-17 20:30:59 +0200

    norme

--- srcs/game/entities/list/pistol/init.c
+++ srcs/game/entities/list/pistol/init.c
@@ -14,8 +14,31 @@
 #include "game/scenes/raycasting_scene.h"
 #include "engine/error.h"
 
-t_result	init_pistol_entity(t_pistol_entity *const self,
-				t_pistol_entity_args *const args)
+static t_result	init_pistol_entity2(t_pistol_entity *const self,
+				t_pistol_entity_args *const args,
+				t_raycasting_scene *ctx)
+{
+	module_add(&self->super.super.super.module, &self->animation,
+		animation(0, 4, 0.0f));
+	if (self->super.super.super.module.has_error)
+	{
+		destroy_pistol_entity(self);
+		return (throw_result_str("init_pistol_entity()",
+			"failed to create pistol animation"));
+	}
+	self->super.super.super.vtable.update = pistol_entity_update;
+	self->super.sound_ref = NULL;
+	self->super.hud_ref = &ctx->assets.pistol_spritesheet;
+	self->super.super.super.transform.position.x = args->pos.x;
+	self->super.super.super.transform.position.y = args->pos.y;
+	self->super.super.super.transform.position.z = 0.0f;
+	self->super.super.super.transform.scale.x = 0.2f;
+	self->super.super.super.transform.scale.y = 0.2f;
+	return (OK);
+}
+
+t_result		init_pistol_entity(t_pistol_entity *const self,
+					t_pistol_entity_args *const args)
 {
 	t_raycasting_scene	*ctx;
 	t_weapon_specs		pistol_specs;
@@ -35,21 +58,5 @@ t_result	init_pistol_entity(t_pistol_entity *const self,
 		return (throw_result_str("init_pistol_entity()",
 			"failed to create parent class"));
 	}
-	module_add(&self->super.super.super.module, &self->animation,
-		animation(0, 4, 0.0f));
-	if (self->super.super.super.module.has_error)
-	{
-		destroy_pistol_entity(self);
-		return (throw_result_str("init_pistol_entity()",
-			"failed to create pistol animation"));
-	}
-	self->super.super.super.vtable.update = pistol_entity_update;
-	self->super.sound_ref = NULL;
-	self->super.hud_ref = &ctx->assets.pistol_spritesheet;
-	self->super.super.super.transform.position.x = args->pos.x;
-	self->super.super.super.transform.position.y = args->pos.y;
-	self->super.super.super.transform.position.z = 0.0f;
-	self->super.super.super.transform.scale.x = 0.2f;
-	self->super.super.super.transform.scale.y = 0.2f;
-	return (OK);
+	return (init_pistol_entity2(self, args, ctx));
 }
