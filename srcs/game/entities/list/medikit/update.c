2550d080e5c19dfe696da71f4ff3c5fb6f8f5eae
Martin RIBOUCHON <mribouch@e1r10p10.42.fr>
2020-08-18 22:49:48 +0200

    += comes +

--- srcs/game/entities/list/medikit/update.c
+++ srcs/game/entities/list/medikit/update.c
@@ -6,7 +6,7 @@
 /*   By: mribouch <mribouch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/16 21:44:06 by lbenard           #+#    #+#             */
-/*   Updated: 2020/08/18 22:43:52 by mribouch         ###   ########.fr       */
+/*   Updated: 2020/08/18 22:49:27 by mribouch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ void	medikit_entity_update(t_medikit_entity *const self)
 		self->super.ctx->entities.player_ref->max_health)
 	{
 		self->super.ctx->entities.player_ref->health = ft_fmin(
-			self->super.ctx->entities.player_ref->health += heal,
+			self->super.ctx->entities.player_ref->health + heal,
 			self->super.ctx->entities.player_ref->max_health);
 		entity_list_remove(&self->super.ctx->super.entities,
 			(t_entity*)self);
