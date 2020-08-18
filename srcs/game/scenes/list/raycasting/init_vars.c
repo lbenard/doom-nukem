a05ed270a461d257e8916159b07dbb069d4d4e4b
Martin RIBOUCHON <mribouch@e1r10p10.42.fr>
2020-08-18 23:11:28 +0200

    a little bit less ammo

--- srcs/game/scenes/list/raycasting/init_vars.c
+++ srcs/game/scenes/list/raycasting/init_vars.c
@@ -6,7 +6,7 @@
 /*   By: mribouch <mribouch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/10 22:20:06 by lbenard           #+#    #+#             */
-/*   Updated: 2020/08/18 22:56:54 by mribouch         ###   ########.fr       */
+/*   Updated: 2020/08/18 23:11:08 by mribouch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	init_raycasting_scene_vars(t_raycasting_scene *const self,
 	self->fov = 90.0f * M_PI / 180.0f;
 	self->window_ref = args->window;
 	self->entities.weapon_ref = NULL;
-	self->weapon.ammo = 36;
+	self->weapon.ammo = 20;
 	self->weapon.last_shot = 0.0;
 	self->weapon.nearest_weapon = NULL;
 	self->tooltips.ending_triggered = FALSE;
