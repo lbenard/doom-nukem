e377894b2f09eae757bd5819041b8088a9c46c9c
Lucas BENARD <lbenard@student.42.fr>
2020-08-19 18:31:27 +0200

    started norm and fixing texture orientation

--- srcs/game/scenes/list/raycasting/cast.c
+++ srcs/game/scenes/list/raycasting/cast.c
@@ -6,7 +6,7 @@
 /*   By: lbenard <lbenard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/10 22:06:52 by lbenard           #+#    #+#             */
-/*   Updated: 2020/07/19 02:23:19 by lbenard          ###   ########.fr       */
+/*   Updated: 2020/08/19 18:31:07 by lbenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,69 +23,82 @@ static t_bool	is_hitting(const t_map *const map, const t_vec2i ray)
 		return (TRUE);
 	return (FALSE);
 }
+
+static void		init_raycast(t_raycast *const self,
+					const t_vec2f pos,
+					const t_vec2f dir)
+{
+	self->delta_distance.x = ft_fabs(1.0f / dir.x);
+	self->delta_distance.y = ft_fabs(1.0f / dir.y);
+	self->ray_position.x = (int)pos.x;
+	self->ray_position.y = (int)pos.y;
+	if (dir.x < 0.0f)
+	{
+		self->step.x = -1;
+		self->distance_from_origin.x = (pos.x - self->ray_position.x)
+			* self->delta_distance.x;
+	}
+	else
+	{
+		self->step.x = 1;
+		self->distance_from_origin.x = (self->ray_position.x + 1.0f - pos.x)
+			* self->delta_distance.x;
+	}
+	if (dir.y < 0.0f)
+	{
+		self->step.y = -1;
+		self->distance_from_origin.y = (pos.y - self->ray_position.y)
+			* self->delta_distance.y;
+	}
+	else
+	{
+		self->step.y = 1;
+		self->distance_from_origin.y = (self->ray_position.y + 1.0f - pos.y)
+			* self->delta_distance.y;
+	}
+}
+
 t_ray	cast(const t_map *const map,
 			const t_vec2f pos,
 			const t_vec2f dir)
 {
-	t_vec2i	ray_pos;
-	t_vec2f	origin_dist;
-	t_vec2f	delta_dist;
-	t_vec2i	step;
+	t_raycast	raycast;
 	t_u8	side;
 	t_bool	hit;
 	float	wall;
 	float	distance;
 	t_ray	result;
 
-	delta_dist = ft_vec2f(ft_fabs(1.0f / dir.x), ft_fabs(1.0f / dir.y));
-	ray_pos = ft_vec2i((int)pos.x, (int)pos.y);
-	if (dir.x < 0.0f)
-	{
-		step.x = -1;
-		origin_dist.x = (pos.x - ray_pos.x) * delta_dist.x;
-	}
-	else
-	{
-		step.x = 1;
-		origin_dist.x = (ray_pos.x + 1.0f - pos.x) * delta_dist.x;
-	}
-	if (dir.y < 0.0f)
-	{
-		step.y = -1;
-		origin_dist.y = (pos.y - ray_pos.y) * delta_dist.y;
-	}
-	else
-	{
-		step.y = 1;
-		origin_dist.y = (ray_pos.y + 1.0f - pos.y) * delta_dist.y;
-	}
+	init_raycast(&raycast, pos, dir);
 	hit = FALSE;
 	while (!hit)
 	{
-		if (origin_dist.x < origin_dist.y)
+		if (raycast.distance_from_origin.x < raycast.distance_from_origin.y)
 		{
-			origin_dist.x += delta_dist.x;
-			ray_pos.x += step.x;
+			raycast.distance_from_origin.x += raycast.delta_distance.x;
+			raycast.ray_position.x += raycast.step.x;
 			side = 0;
 		}
 		else
 		{
-			origin_dist.y += delta_dist.y;
-			ray_pos.y += step.y;
+			raycast.distance_from_origin.y += raycast.delta_distance.y;
+			raycast.ray_position.y += raycast.step.y;
 			side = 1;
 		}
-		hit = is_hitting(map, ray_pos);
+		hit = is_hitting(map, raycast.ray_position);
 	}
 	if (side == 0)
-		distance = (ray_pos.x - pos.x + (1.0f - step.x) / 2.0f) / dir.x;
+		distance = (raycast.ray_position.x - pos.x + (1.0f - raycast.step.x) / 2.0f) / dir.x;
 	else
-		distance = (ray_pos.y - pos.y + (1.0f - step.y) / 2.0f) / dir.y;
+		distance = (raycast.ray_position.y - pos.y + (1.0f - raycast.step.y) / 2.0f) / dir.y;
 	if (side == 0)
 		wall = pos.y + distance * dir.y;
 	else
 		wall = pos.x + distance * dir.x;
-	result.hit = ft_vec2f(ray_pos.x, ray_pos.y);
+	result.hit = ft_vec2f(raycast.ray_position.x, raycast.ray_position.y);
 	result.perpendicular_distance = distance;
 	result.horizontal_ratio = wall - (int)wall;
+	if (side == 0 && raycast.step.x == -1)
+		result.horizontal_ratio = 1.0f - result.horizontal_ratio;
 	return (result);
 }
