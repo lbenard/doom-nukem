eca966206d76c28b0ebf70bc0338cedea8606a70
Lucas BENARD <lbenard@student.42.fr>
2020-08-19 04:58:43 +0200

    libft norm

--- libft/srcs/containers/module/ft_constructor.c
+++ libft/srcs/containers/module/ft_constructor.c
@@ -6,13 +6,13 @@
 /*   By: lbenard <lbenard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/06 02:26:01 by lbenard           #+#    #+#             */
-/*   Updated: 2019/11/06 02:59:02 by lbenard          ###   ########.fr       */
+/*   Updated: 2020/08/19 04:58:09 by lbenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "containers/module.h"
 
-t_constructor	ft_constructor(t_result (*const constructor_fn)(),
+t_constructor	ft_constructor(enum e_result (*const constructor_fn)(),
 					void (*const destructor_fn)(),
 					const size_t module_size,
 					const void *const args)
